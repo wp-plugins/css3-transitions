b0990e5260fb995accb532850202f8d1f994d823
celloexpressions <celloexpressions@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-07-01 22:37:09 +0000

    Push 1.3 update
    git-svn-id: https://plugins.svn.wordpress.org/css3-transitions/trunk@734860 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- css3-transitions.php
+++ css3-transitions.php
@@ -3,7 +3,7 @@
  * Plugin Name: CSS3 Transitions
  * Plugin URI: http://celloexpressions.com/dev/css3-transitions
  * Description: Automatically adds CSS3 transitions to your website/blog and the WordPress admin. Links, etc. get animated transitions between their normal and hover states.
- * Version: 1.2
+ * Version: 1.3
  * Author: Nick Halsey
  * Author URI: http://celloexpressions.com/
  * Tags: css3, transitions, eye candy, ui, ux, effects, smooth, automatic, auto, animate, animations
@@ -28,36 +28,27 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 =====================================================================================
 */
 
-add_action('wp_head', 'do_css3_transitions');
-add_action('admin_head', 'do_css3_transitions');
+add_action('wp_head', 'cxnh_css3_transitions');
+add_action('admin_head', 'cxnh_css3_transitions');
 
-function do_css3_transitions(){ ?>
+function cxnh_css3_transitions(){ ?>
 	<style type="text/css">
 	/* From CSS3 Transitions Plugin.
 		target several selectors which are most likely to have 
-		hover states defined in the theme (and/or the WP core)
+		hover states defined in the theme
 	*/
 		a, li {
-			/* Unfortunately, it doesn't seem posible to do exclude background-property from all, so just don't use any sprites please... */
-			transition: all ease .3s;
-			-webkit-transition: all ease .3s;
-			-ms-transition: all ease .3s;
-			-moz-transition: all ease .3s;
-			-o-transition: all ease .3s;
+			/* Unfortunately, it doesn't seem posible to do exclude background-position from all, so just don't use any sprites */
+			transition: all .18s cubic-bezier(0.64,0.20,0.02,0.35);
+			-webkit-transition: all .18s cubic-bezier(0.64,0.20,0.02,0.35);
 		}
 		img {
-			transition: all ease .5s;
-			-webkit-transition: all ease .5s;
-			-ms-transition: all ease .5s;
-			-moz-transition: all ease .5s;
-			-o-transition: all ease .5s;
+			transition: all .3s cubic-bezier(0.64,0.20,0.02,0.35);
+			-webkit-transition: all .3s cubic-bezier(0.64,0.20,0.02,0.35);
 		}
 		input, textarea, button, label, option, select, .button, .hndle {
-			transition: all ease .4s;
-			-webkit-transition: all ease .4s;
-			-ms-transition: all ease .4s;
-			-moz-transition: all ease .4s;
-			-o-transition: all ease .4s;
+			transition: all .25s cubic-bezier(0.64,0.20,0.02,0.35);
+			-webkit-transition: all .25s cubic-bezier(0.64,0.20,0.02,0.35);
 		}
 		
 		/* transitions mess all of these up in the WordPress Core */
@@ -65,10 +56,7 @@ function do_css3_transitions(){ ?>
 		.wp-picker-holder a {
 			transition: none;
 			-webkit-transition: none;
-			-ms-transition: none;
-			-moz-transition: none;
-			-o-transition: none;
 		}
-	</style>	
+	</style>
 <?php }
 // Yup, that's it!
